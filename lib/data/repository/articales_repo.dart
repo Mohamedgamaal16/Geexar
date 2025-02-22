708ca826f4a8279d57965fba711ba88d4613b7f7
Mohamedgamaal16 <mahooomar@yahoo.com>
2025-02-22 12:35:57 +0200

    remove duplictation

--- lib/data/repository/articales_repo.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:geexar/data/models/article_model.dart';
-
-abstract class ArticlesRepo {
-  Future<Either<List<Article>, String>> fetchArticles(String query);
-}
